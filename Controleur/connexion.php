747ee88a8ac87886ebfbbfa298716dfb1df40f2a
Clément <cle.coulon@gmail.com>
2018-12-14 18:42:59 +0100

    correctif login faille

--- Controleur/connexion.php
+++ Controleur/connexion.php
@@ -1,10 +1,19 @@
 <?php
 
     require_once('..\Modele\membre.php');
+    require_once('..\Modele\bdd.php'); 
+
+    $bd = new bd(); 
+    $bd->connect(); 
+    $co = $bd->getConnexion() ; 
+   
     
-    $login = $_POST['login'];
-    $mdp = $_POST['mdp']; 
+    $login =htmlspecialchars(htmlentities(strip_tags( $_POST['login'])));
+    $mdp = htmlspecialchars(htmlentities(strip_tags($_POST['mdp']))); 
     
+    $login=$co->real_escape_string($login);
+    $mdp=$co->real_escape_string($mdp);
+
     if(verifInfos($login, $mdp)){
         connexion($login, $mdp); 
         echo "Success !";
