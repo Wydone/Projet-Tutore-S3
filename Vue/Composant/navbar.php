d87b1f59d673933b3df08555bec12b5fbb8b1803
jovenium <lucas1789l@gmail.com>
2018-12-27 11:20:25 +0100

    navbar non connecter ! ajout de getSesGroupesMembre dans membre.php

---
+++ Vue/Composant/navbar.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="accueil.php"><img class="logo" src="../Image/logo.png"/></a>
+
+<!--icon site-->
+  <button class="navbar-toggler" type="button" data-toggle="collapse"
+			data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+			aria-label="Toggle navigation">
+   	<span class="navbar-toggler-icon"></span>
+  </button>
+
+<!--menu gauche-->
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Fonctionnalitées</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+
+<?php echo 'hello'; ?>
