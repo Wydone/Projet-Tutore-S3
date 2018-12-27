a8dde5de36bfe9bfb2d0594c3f66d3bfd033d516
jovenium <lucas1789l@gmail.com>
2018-12-27 12:50:14 +0100

    ajout navbar connect et composant

---
+++ Vue/Composant/meta.php
@@ -0,0 +1,5 @@
+<link href="https://fonts.googleapis.com/css?family=Nanum+Gothic" rel="stylesheet">
+
+<link rel="stylesheet" href="../Style/bootstrap.min.css">
+
+<link rel="stylesheet" href="../Style/style.css">
--- Vue/mon_profil.php
+++ Vue/mon_profil.php
@@ -7,20 +7,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="main.js"></script>
+
+    <?php include'Composant/meta.php';?>
 </head>
 <body>
 
+    <?php include'Composant/navbar_connect.php';?>
+
     <h1>Mes infos</h1>
-    <?php 
-        
+    <?php
+
         if(!isset($_SESSION['nom'])){
-            echo "error"; 
+            echo "error";
         }else {
             echo $_SESSION['email']."<br>".$_SESSION['nom']."<br>".$_SESSION['prenom'];
         }
 
         if(isset($mdpModifie)){
-            echo "<br>mot de passe modifié <br>"; //gérer cette option 
+            echo "<br>mot de passe modifié <br>"; //gérer cette option
         }
     ?>
             <form action="..\Controleur\modifier_mdp.php" method="post">
@@ -30,18 +34,18 @@
                 <label for ="verifmdp"> Confirmer mot de passe :  </label>
                 <input type="password" name="verfimdp" value=""/>
                 <br>
-    
+
                 <input type="submit" value="Modifier"/>
             </form>
-    
+
     <h1>Mes souhaits</h1>
-    <?php 
-        
+    <?php
+
         foreach($sesCadeaux as $cadeau){
-            
-            echo $cadeau->getNom()." : ".$cadeau->getDesc()."<br>" ; 
+
+            echo $cadeau->getNom()." : ".$cadeau->getDesc()."<br>" ;
         }
-        
+
     ?>
             <h2>Ajouter un cadeau à ma liste </h2>
             <form action="..\Controleur\ajouter_cadeau.php" method="post" enctype="multipart/form-data" >
@@ -60,13 +64,15 @@
                 <input type="submit" value="Ajouter"/>
             </form>
     <h1>Groupes que j'administre</h1>
-    <?php 
-        
+    <?php
+
         foreach($sesGroupesAdmin as $groupe){
-            
-            echo "groupe : ".$groupe->getNom()."<br>" ; 
+
+            echo "groupe : ".$groupe->getNom()."<br>" ;
         }
-        
+
     ?>
+
+      <?php include'Composant/body_script.php';?>
 </body>
 </html>
