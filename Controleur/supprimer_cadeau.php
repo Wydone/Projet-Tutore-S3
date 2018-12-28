9f6f2bff25f6adfca85e3d956bca686f11277c93
Clément <cle.coulon@gmail.com>
2018-12-28 13:24:46 +0100

    fix cadeaeu suppression v2

--- Controleur/supprimer_cadeau.php
+++ Controleur/supprimer_cadeau.php
@@ -10,8 +10,6 @@
     $m = new Membre($_SESSION['nom'], $_SESSION['prenom'], $_SESSION['email'], $_SESSION['login'], $_SESSION['mdp'], $_SESSION['id']);
     $co = $m->getCo(); 
     
-    $id = $_SESSION['idCadeauSupprime']; 
-    echo $id ; 
     $sesCadeaux = $m->supprimerCadeau($_SESSION['idCadeauSupprime']); 
     $sesGroupesAdmin = $m->getSesGroupesAdmin() ; 
     
