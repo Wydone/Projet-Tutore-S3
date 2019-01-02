c515e399354baa5a8f2fc8f21461c93b49965b8e
jovenium <lucas1789l@gmail.com>
2019-01-02 15:07:13 +0100

    mes groupes addaptatif

--- Vue/mes_groupes.php
+++ Vue/mes_groupes.php
@@ -2,31 +2,62 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Accueil</title>
-    <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic" rel="stylesheet">
+    <title>Mes Groupes</title>
 
-    <link rel="stylesheet" href="../Style/bootstrap.min.css">
-
-    <link rel="stylesheet" href="../Style/style.css">
+    <?php include'Composant/meta.php';?>
   </head>
-  <body>
-
-	<?php include'Composant/navbar_connect.php';?>
-
-
-
-	<h1>test</h1>
+  <body class="full">
+  <?php include'Composant/class.php';//contient les class du modele ?>
+	<?php include'Composant/navbar_connect.php';//contient la session start?>
 
 
+<section class="padding-page full-screen">
+
+  <div class="fluid-container full-screen">
+    <div class="row margin-zero full-screen">
+      <div class="col-lg-3 groupes full-div">
+          <a href="#">
+            <div class="un-groupe">
+              <h2>Nom groupe</h2>
+            </div>
+          </a>
+          <a href="#">
+            <div class="un-groupe">
+                <h2>Nom groupe</h2>
+            </div>
+          </a>
+          <a href="#">
+            <div class="un-groupe">
+                <h2>Nom groupe</h2>
+            </div>
+          </a>
 
 
-	<?php include'Composant/footer.php';?>
 
+
+          <button>
+            <div class="un-groupe">
+                <h2>Nom groupe</h2>
+            </div>
+          </button>
+      </div>
+      <div class="col membres">
+        <div class="un-membre">
+              <h2>test</h2>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+</section>
+
+
+<?php include'Composant/footer.php';?>
 
 
   <!--JavScript and Jquery-->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+  <?php include'Composant/body_script.php';?>
   </body>
 </html>
