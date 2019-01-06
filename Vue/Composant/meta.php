467c3070b35dbb1617cdf3ae4115d0468515e568
jovenium <lucas1789l@gmail.com>
2019-01-06 15:13:44 +0100

    icon fix dl

--- Vue/Composant/meta.php
+++ Vue/Composant/meta.php
@@ -3,7 +3,7 @@
 <link rel="stylesheet" href="../Style/bootstrap.min.css">
 
 <link rel="stylesheet" href="../Style/style.css">
-
+<link rel="stylesheet" href="../Style/fontawesome-free-5.6.3-web/css/all.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">-->
